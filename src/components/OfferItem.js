6b30115830239ef06526cbf552eebbbe1995cc5e
Lorax6 <tiasajeev6@gmail.com>
2025-08-22 15:03:54 -0700

    Content in OfferItem

--- src/components/OfferItem.js
+++ src/components/OfferItem.js
@@ -4,10 +4,12 @@ import React from 'react'
 const OfferItem = () => {
   return (
     <div>
-      <div className="offerTitle">Skill building workshops</div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos enim, quisquam, labore, 
+      <div className="offer-box">
+        <h3>Skill building workshops</h3>                            
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos enim, quisquam, labore, 
         animi sed explicabo commodi maxime deleniti cum omnis! Assumenda quo laboriosam impedit,
          sunt repellendus placeat expedita ut.</p>
+      </div>
     </div>
   )
 }
