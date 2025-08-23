c853b58a8dfabb813fc151ea090a9de4beaf1f6f
Lorax6 <tiasajeev6@gmail.com>
2025-08-22 20:55:15 -0700

    Added offer components and description

--- src/components/WhatWeOffer.js
+++ src/components/WhatWeOffer.js
@@ -4,11 +4,11 @@ import OfferItem from './OfferItem'
 const WhatWeOffer = () => {
   return (
     <div>
-      <div className="we-offer">
-        <h2>What We Offer</h2>
-        <OfferItem/>
-        <OfferItem/>
-        <OfferItem/>
+     <h2 className='offer-header'>What We Offer</h2>
+      <div className="offer">
+        <OfferItem icon= "🎯" title="Skill building workshops" description="Master data interpretation, financial modeling, and behavioral insights"/>
+        <OfferItem icon="🏆" title="Mock Case competitions" description="Stimulate real consulting challenges with timed cases and feedback from industry experts"/>
+        <OfferItem icon="🤝" title="Networking and Mentorship" description="Connect with alumni and consultants from local firms through guest speaker sessions"/>
       </div>
     </div>
   )
