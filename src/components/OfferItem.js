dea6ccec1c3a64c100b38d9ca6d132df465a6689
Lorax6 <tiasajeev6@gmail.com>
2025-08-22 20:54:12 -0700

    Added props & styling for offer section

--- src/components/OfferItem.js
+++ src/components/OfferItem.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 
-const OfferItem = () => {
+const OfferItem = ({icon,title,description}) => {
   return (
     <div>
       <div className="offer-box">
-        <h3>Skill building workshops</h3>                            
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos enim, quisquam, labore, 
-        animi sed explicabo commodi maxime deleniti cum omnis! Assumenda quo laboriosam impedit,
-         sunt repellendus placeat expedita ut.</p>
+        {icon && <div className="offer-icon">{icon}</div>}
+        <h3>{title}</h3>                            
+        <p>{description}</p>
       </div>
     </div>
   )
