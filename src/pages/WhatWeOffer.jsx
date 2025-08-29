9456526c347e74028fd55b916b87d47cea392274
Lorax6 <tiasajeev6@gmail.com>
2025-08-29 10:02:04 -0700

    changed from js to jsx

---
+++ src/pages/WhatWeOffer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import OfferItem from '../components/OfferItem'
+
+const WhatWeOffer = () => {
+  return (
+    <div>
+     <h2 className="section-title">What We Offer</h2>
+      <div className="offer">
+        <OfferItem icon= "🎯" title="Skill building workshops" description="Master data interpretation, financial modeling, and behavioral insights"/>
+        <OfferItem icon="🏆" title="Mock Case competitions" description="Stimulate real consulting challenges with timed cases and feedback from industry experts"/>
+        <OfferItem icon="🤝" title="Networking and Mentorship" description="Connect with alumni and consultants from local firms through guest speaker sessions"/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatWeOffer
