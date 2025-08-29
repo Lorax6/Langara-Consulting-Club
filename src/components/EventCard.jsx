a94c1e41c711cf355f3d3635c689eed987f1ce78
Lorax6 <tiasajeev6@gmail.com>
2025-08-29 09:56:22 -0700

    changed from js to jsx

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const EventCard = ({eventImg,eventTitle, date,time,location,eventDesc }) => {
+  return (
+    <div>
+      <div className="event-box">
+        <img className="event-img" src={eventImg} alt='Event Thumbnail' width={100} height={50} />
+        <h3>{eventTitle}</h3> 
+        <p>{"📆" + date}</p>                           
+        <p>{"⌚" + time}</p>                           
+        <p>{"📍"+location}</p>                           
+        <p className="description">{eventDesc}</p>
+        <button className="register-button">Register</button>
+      </div>
+    </div>
+  )
+}
+
+export default EventCard
