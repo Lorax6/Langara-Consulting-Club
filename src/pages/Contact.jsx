ebef954445513fa4e032862a576be4fcf0499004
Tia <tsajeev@students.cs.ubc.ca>
2025-09-23 13:26:22 -0700

    added section id

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,26 +32,28 @@ const Contact = () => {
 
 
   return (
-    <div>
-      <h2 className="section-title">Contact Us</h2>
-      <div className='contact'>
-        <form onSubmit={onSubmit} className='contact-form'>
-            <div className="input-box">
-                <label>Full Name</label>
-                <input type="text" className='field' placeholder='Enter your name' name='name' required />
-            </div>
-            <div className="input-box">
-                <label>Email Address</label>
-                <input type="email" className='field' placeholder='Enter your email' name='email' required />
-            </div>
-            <div className="input-box">
-                <label>Your message</label>
-                <textarea name="message" id="" className='field msg' placeholder='Enter your message' required></textarea>
-            </div>
-            <button type='submit' className='contact-sub'>Submit</button>
-        </form>
-      </div>
-    </div>
+    <section id='contact'>
+        <div>
+        <h2 className="section-title">Contact Us</h2>
+        <div className='contact'>
+            <form onSubmit={onSubmit} className='contact-form'>
+                <div className="input-box">
+                    <label>Full Name</label>
+                    <input type="text" className='field' placeholder='Enter your name' name='name' required />
+                </div>
+                <div className="input-box">
+                    <label>Email Address</label>
+                    <input type="email" className='field' placeholder='Enter your email' name='email' required />
+                </div>
+                <div className="input-box">
+                    <label>Your message</label>
+                    <textarea name="message" id="" className='field msg' placeholder='Enter your message' required></textarea>
+                </div>
+                <button type='submit' className='contact-sub'>Submit</button>
+            </form>
+        </div>
+        </div>
+    </section>
   )
 }
 
