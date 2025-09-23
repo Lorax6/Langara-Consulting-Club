d4dae8e02d704c97f127df1cc608e508527cc803
Tia <tsajeev@students.cs.ubc.ca>
2025-09-23 13:26:02 -0700

    Added tabs for event

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const EventCard = ({eventImg,eventTitle, date,time,location,eventDesc }) => {
+const EventCard = ({eventImg,eventTitle, date,time,location,eventDesc, eventLink }) => {
   return (
     <div>
       <div className="event-box">
-        <img className="event-img" src={eventImg} alt='Event Thumbnail' width={100} height={50} />
+        <img className="event-img" src={eventImg} alt='Event Thumbnail' width={170} height={90} />
         <h3>{eventTitle}</h3> 
         <p>{"📆" + date}</p>                           
         <p>{"⌚" + time}</p>                           
         <p>{"📍"+location}</p>                           
         <p className="description">{eventDesc}</p>
-        <button className="register-button">Register</button>
+        <a href={eventLink} target='_blank' rel='noopener noreferrer'>
+            <button className="register-button">Register</button>
+        </a>
+       
       </div>
     </div>
   )
